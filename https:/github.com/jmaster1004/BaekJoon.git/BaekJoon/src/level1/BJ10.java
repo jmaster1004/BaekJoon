403d2ff220c0d9c56482dee6d692880ccbbb9c49
jmaster1020 <jmaster1020@gmail.com>
2022-02-01 00:02:00 +0900

    ㅇㄴㅇ

---
+++ https:/github.com/jmaster1004/BaekJoon.git/BaekJoon/src/level1/BJ10.java
@@ -0,0 +1,15 @@
+package level1;
+
+import java.util.Scanner;
+
+public class BJ10 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		int x = 543;	
+		
+		System.out.println(y-x);
+		
+	}
+}
