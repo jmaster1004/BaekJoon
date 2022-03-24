315e528aa157e63f203bad6c1d5e0f1da4d0a365
jmaster1020 <jmaster1020@gmail.com>
2022-03-24 09:36:48 +0900

    BJ11022

---
+++ BaekJoon/src/level3/BJ11022.java
@@ -0,0 +1,31 @@
+package level3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BJ11022 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int C = A+B;
+			
+			bw.write("Case #"+i+": "+A+" + "+B+" = "+C+"\n");
+		}
+		
+		br.close();
+		bw.flush();
+		br.close();
+	}
+}
