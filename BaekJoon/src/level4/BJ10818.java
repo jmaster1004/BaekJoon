b5a135a7626e202b47b8fc3f95147f6430442617
jmaster1020 <jmaster1020@gmail.com>
2022-04-04 13:21:00 +0900

    BJ10818

---
+++ BaekJoon/src/level4/BJ10818.java
@@ -0,0 +1,34 @@
+package level4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import javax.naming.ldap.SortControl;
+
+
+public class BJ10818 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int[] array = new int[N];
+		br.close();
+		
+		
+		for(int i=0; i<N; i++) { 
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(array);
+        System.out.println(array[0]+" "+array[N-1]);
+	}
+}
